be13ef57519242f11c9912bbd37d06be9a521579
Narongdaet Data <6330146421@student.chula.ac.th>
2022-05-29 01:24:34 +0700

    fix css bug

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express'),
     app = express(),
     request = require('request'),
     mongoose = require('mongoose'),
-    path = require('path')
+    path = require('path'),
+    cron = require('node-cron')
     
 
     monconnect = mongoose.connect('mongodb+srv://embedded-final:embedded-final@final.srrbpje.mongodb.net/stm?retryWrites=true&w=majority')
@@ -19,18 +20,42 @@ var time = -1
 const nodeIp = 'http://192.168.179.206/'
 const curIp = 'http://192.168.79.86/'
 
-// const isActive = mongoose.model('isActive', new mongoose.Schema({
-//     isActive : {
-//         type : Boolean
-//     }
+const data = mongoose.model('data', new mongoose.Schema({
+    humid : {
+        type : Number
+    },
+    light : {
+        type : Number
+    },
+    water : {
+        type : Number
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    }
     
-// }, {database: "stm", collection:'isActive'}));
+}, {database: "stm", collection:'data'}));
 
 // console.log(isActive.findById("628f90eee7f31d2861ef614b"))
 
 
 
 
+
+cron.schedule("* */1 * * * *", ()=> {
+    request(nodeIp+'getInfo', { json: true }, async (err, response, body) => {
+            await data.create({
+                humid: response.body.humid,
+                light: response.body.light,
+                water: response.body.water
+
+            })
+            
+            
+        });
+
+})
     
 app.set('port',80);
 const http = require('http');
@@ -39,27 +64,26 @@ const server = http.createServer(app);
 
 function getTimeLeft(te){
     if( te === -1) return -1;
-    
     return te - new Date().getTime()
 }
-app.get('/',   (req,res) => {
+
+app.get('/',  async (req,res) => {
     // iAT =   isActive.findById("628f90eee7f31d2861ef614b", (err,doc)=>{
     //     if(err) console.log(err);
     //     else console.log(doc)
     // })
-    res.render('index', {isActive: isActive,isDisabled:isDisabled, timeout:getTimeLeft(timeend)})
+    var latest = await data.find({}).sort({time:-1}).limit(1)
+    
+    res.render('index', {isActive: isActive,isDisabled:isDisabled, timeout:getTimeLeft(timeend), latest:latest})
     // console.log(iAT)
 })
 
-app.post('/getInfo', (req,res) => {
-    request(nodeIp+'getInfo', { json: true }, (err, response, body) => {
-    if (err) { return res.json({status:'error', code :"hee"}); }
-        // console.log(response);
-        // console.log(body.explanation);
-        res.json({status:'ok', body:response.body})
-    });
 
+app.post('/getInfo', async (req,res) => {
+    res.json(await data.find({}).sort({time:-1}).limit(1))
 })
+
+
 
 app.post('/water', (req,res) => {
     if(isDisabled) return res.json({status:'error'})
@@ -85,8 +109,22 @@ app.post('/water', (req,res) => {
 })
 
 
-app.get('/log',   (req,res) => {
-    res.render('logdisp');
+app.get('/log',   async (req,res) => {
+    var id = req.query.id
+    var data_page = await data.aggregate([
+        {
+            $sort:{time:-1}
+        },
+        {
+            $skip:Number(id)
+        },
+        {
+            $limit: 9
+        }
+    ])
+    var val = await data.count();
+
+    res.render('logdisp', {data: data_page, id:id, DocSize: val});
 })
 
 app.post('/valveOn', (req,res) => {
@@ -106,10 +144,8 @@ app.post('/valveOn', (req,res) => {
 app.post('/valveOff', (req,res) => {
     if(isActive = false){res.json({status:'error'}); return}
     request(nodeIp+'valveOff',  function (error, response, body) {
-        
             //  isActive.findByIdAndUpdate("628f90eee7f31d2861ef614b", {isActive: false}, (err,rs) => {
             //     if(err) console.log(err);
-                
             // })
             isActive = false;
             res.json({status:'ok'})
